c7f85068859d35654e386df79e5a9e1dbb647493
jardeleko <jardelduarte594@gmail.com>
2023-02-16 19:41:38 -0300

    carnival content

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>E aí, vamo fecha?</h1>
+          <img style={{alignItems: 'center', justifyContent:'center', marginLeft: '30px'}} src='https://thumbs.gfycat.com/FeminineSoulfulGannet-max-1mb.gif' alt='single gif'/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
